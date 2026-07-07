e45bafae73a5ac3cfbb25e626fb27f2cf13f865b
JosephWilson <jwilson@staudertech.com>
2026-07-07 17:28:37 -0500

    basic weather API setup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+// Top Left Clock: uses new Date
 function digitalClock() {
     const now = new Date();
 
@@ -22,6 +24,7 @@ function digitalClock() {
     document.getElementById("clockTime").innerText = timeString;
 }
 
+// Top Left Quote: Pulls from data/quotes.json
 function loadQuote() {
     fetch("data/quotes.json")
         .then(response => response.json())
@@ -36,17 +39,34 @@ function loadQuote() {
         });
 }
 
-// TODO: Weather bellow
-// const url = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city)}&count=1`; for geocode
-// https://open-meteo.com/en/docs
+// Gets Lat/Long: Fetches from API
 async function geoCode() {
     const city = "ROLLA"
-    const url = `https://geocoding-api.open-meteo.com/v1/search?
-    name=${encodeURIComponent(city)}&count=10&language=en&format=json`;
+    const url = `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city)}&count=10&language=en&format=json`;
     const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+
+    const location = data.results[0];
+    return location;
 }
 
+// Top Right Weather: blahblahblah
+async function getWeather() {
+    const location = await geoCode();
+    const lat = location.latitude;
+    const lon = location.longitude;
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=uv_index&minutely_15=temperature_2m,weather_code,relative_humidity_2m`;
+
+    const res = await fetch(url);
+    const data = await res.json()
+    console.log(data);
+    
+}
+
+getWeather();
+setInterval(getWeather, 900001); // 15 Minutes
 digitalClock();
-setInterval(digitalClock, 1000);
+setInterval(digitalClock, 1000); // Seconds
 loadQuote();
-setInterval(loadQuote, 14400000);
+setInterval(loadQuote, 14400000); // Days
